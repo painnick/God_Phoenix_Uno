24b26c419f65a3b9d84256f6534a0fabfb494491
painnick <painnick@gmail.com>
2021-12-24 22:25:00 +0900

    CHANGE. Normal일 때 처리 변경

--- GodPhoenix_Xiao.ino
+++ GodPhoenix_Xiao.ino
@@ -1,6 +1,6 @@
 /**
  * God Phoenix L.E.D work
- * 
+ *
  * @author painnick@gmail.com
  * @see https://smile-dental-clinic.info/wordpress/?p=11441
  **/
@@ -12,25 +12,29 @@
 #define _DEBUG 1
 
 #ifdef _DEBUG
-#define STRIP_BRIGHT 5
+#define STRIP_BRIGHT 10
+#define VOLUME 10
 #else
 #define STRIP_BRIGHT 250
+#define VOLUME 20
 #endif
 
-#define HEAD_PIN 0     // #1 기수 앞 부분의 NeoPixel
-#define COCKPIT_PIN 1  // #2 콕핏 부분의 NeoPixel
-#define TOP_PIN 2      // #3 상단의 원형 클리어 안에 심은 LED
+#define HEAD_PIN 0    // #1 기수 앞 부분의 NeoPixel
+#define COCKPIT_PIN 1 // #2 콕핏 부분의 NeoPixel
+#define TOP_PIN 2     // #3 상단의 원형 클리어 안에 심은 LED
 #define RX_PIN 3
 #define TAILSIDE_PIN 4 // #5 엔진 좌우의 클리어 안에 심은 LED
 #define ENGINE_PIN 5   // #6 엔진 클리어 안에 심은 LED
 #define TX_PIN 6
-#define BUTTON_PIN 8   // #9 신호 입력용 버튼 스위치
+#define BUTTON_PIN 8 // #9 신호 입력용 버튼 스위치
+
+#define COLOR_BLACK 0x0
 
 Adafruit_NeoPixel head_strip = Adafruit_NeoPixel(11 * 2, HEAD_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel cockpit_strip = Adafruit_NeoPixel(7, COCKPIT_PIN, NEO_GRB + NEO_KHZ800);
 
-class Mp3Notify; 
-typedef DFMiniMp3<SoftwareSerial, Mp3Notify> DfMp3; 
+class Mp3Notify;
+typedef DFMiniMp3<SoftwareSerial, Mp3Notify> DfMp3;
 SoftwareSerial mySerial(RX_PIN, TX_PIN);
 DfMp3 dfmp3(mySerial);
 
@@ -56,77 +60,199 @@ void setup()
 #endif
 }
 
-#ifdef _DEBUG
-int lastState = -1;
-#endif
+#define PROCESSOR_NORMAL 0
+#define PROCESSOR_PHOENIX 1
+
+int processor = -1;
+int normalStep = -1;
+int phoenixStep = -1;
 void loop()
 {
-// #ifdef _DEBUG
-//   Serial.println("===== Loop start =====");
-// #endif
+  waitMilliseconds(500);
 
-  dfmp3.loop(); 
-  delay(1);
-
-  if (digitalRead(BUTTON_PIN) == LOW)
+  int newState = digitalRead(BUTTON_PIN);
+  switch (processor)
   {
-    if (lastState != LOW) {
+  case PROCESSOR_NORMAL: // == LOW
+    if (newState == HIGH)
+    {
+#ifdef _DEBUG
+      Serial.println("Button On");
+#endif
+      processor = PROCESSOR_PHOENIX;
+      phoenixStep = 0;
+    }
+    break;
+  case PROCESSOR_PHOENIX: // == HIGH
+    if (newState == LOW)
+    {
 #ifdef _DEBUG
       Serial.println("Button Off");
 #endif
-      normal_form();
+      processor = PROCESSOR_NORMAL;
+      normalStep = 0;
     }
-    lastState = LOW;
-  }
-  else
-  {
-    if (lastState != HIGH) {    
+    break;
+  default:
+    if (newState == LOW)
+    {
 #ifdef _DEBUG
-      Serial.println("Button On - Phoenix!!!");
+      Serial.println("Button Off(First)");
 #endif
-      phoenix_form();
+      processor = PROCESSOR_NORMAL;
+      normalStep = 0;
     }
-    lastState = HIGH;
+    else
+    {
+#ifdef _DEBUG
+      Serial.println("Button On(First)");
+#endif
+      processor = PROCESSOR_PHOENIX;
+      phoenixStep = 0;
+    }
+    break;
   }
 
-// #ifdef _DEBUG
-//   delay(1000);
+  switch (processor)
+  {
+  case PROCESSOR_NORMAL:
+    normal_form(normalStep);
+    normalStep++;
+    break;
+  case PROCESSOR_PHOENIX:
+    // @TODO.
+    phoenixStep++;
+    break;
 
-//   Serial.println("----- Loop end -----");
-// #endif
+  default:
+    break;
+  }
+}
+
+void waitMilliseconds(uint16_t msWait)
+{
+  uint32_t start = millis();
+
+  while ((millis() - start) < msWait)
+  {
+    // if you have loops with delays, its important to
+    // call dfmp3.loop() periodically so it allows for notifications
+    // to be handled without interrupts
+    dfmp3.loop();
+    delay(1);
+  }
 }
 
 //----------------------------------------------------------------------------------
 /**
  * 노말 모드
- * NeoPixel은 1가지 색으로 설정하고, 일반 LED의 밝기는 다소 어두운 상태로 켠다
  **/
-void normal_form()
+uint32_t normalColor = 0x0000;
+void normal_form(int &step)
 {
+  switch (step)
+  {
+  case 0: // mp3 재생 시작
 #ifdef _DEBUG
-      Serial.println("Process - Normal");
+    Serial.println("Process - Normal");
 #endif
 
-#ifdef _DEBUG
-  dfmp3.setVolume(10);
-#else
-  dfmp3.setVolume(20);
-#endif
-  delay(100);
+    dfmp3.setVolume(VOLUME);
+    waitMilliseconds(100);
 
-  dfmp3.playFolderTrack16(1, 1);
-  delay(100);
+    dfmp3.playFolderTrack16(1, 1);
+    waitMilliseconds(100);
 
-  analogWrite(TOP_PIN, 50);
+    break;
+  case 1:
+    analogWrite(TOP_PIN, 50);
+    after_burner(ENGINE_PIN, TAILSIDE_PIN, false);
 
-  after_burner(ENGINE_PIN, TAILSIDE_PIN, false);
+    head_strip.setBrightness(STRIP_BRIGHT);
+    cockpit_strip.setBrightness(STRIP_BRIGHT);
 
-  head_strip.setBrightness(5);
-  cockpit_strip.setBrightness(10);
+    normalColor = head_strip.Color(255, 140, 0);
+    // normalColor = head_strip.Color(random(63, 191), random(63, 191), random(63, 191));
 
-  uint32_t normalColor = head_strip.Color(255, 140, 0);
-  colorWipe(&head_strip, normalColor, 10, true);
-  colorWipe(&cockpit_strip, normalColor, 10, false);
+    // 3번 깜박임. 종료 시점에 불이 켜져 있는 상태
+    for (int i = 0; i < 6; i++)
+    {
+      if (i % 2 == 0)
+      {
+        // Screen Off
+        colorWipe(&head_strip, COLOR_BLACK, 10, true);
+        colorWipe(&cockpit_strip, COLOR_BLACK, 10, false);
+      }
+      else
+      {
+        colorWipe(&head_strip, normalColor, 30, true);
+        colorWipe(&cockpit_strip, normalColor, 30, false);
+      }
+      waitMilliseconds(200);
+    }
+    break;
+  case 2:
+    for (int i = 0; i < 6; i++)
+    {
+      COLOR_WIPE_TYPE targetType = COLOR_WIPE_TYPE::ALL;
+      COLOR_WIPE_TYPE blackType = COLOR_WIPE_TYPE::ALL;
+      switch (i % 2)
+      {
+      case 0:
+        targetType = COLOR_WIPE_TYPE::ODD;
+        blackType = COLOR_WIPE_TYPE::EVEN;
+        break;
+      case 1:
+        targetType = COLOR_WIPE_TYPE::EVEN;
+        blackType = COLOR_WIPE_TYPE::ODD;
+        break;
+      }
+
+      // Screen Off
+      colorWipe(&head_strip, COLOR_BLACK, 1, true, blackType);
+      colorWipe(&cockpit_strip, COLOR_BLACK, 1, false, blackType);
+
+      colorWipe(&head_strip, normalColor, 1, true, targetType);
+      colorWipe(&cockpit_strip, normalColor, 1, false, targetType);
+
+      waitMilliseconds(200);
+    }
+
+    // Screen Off
+    colorWipe(&head_strip, COLOR_BLACK, 1, true, COLOR_WIPE_TYPE::ALL);
+    colorWipe(&cockpit_strip, COLOR_BLACK, 1, false, COLOR_WIPE_TYPE::ALL);
+
+    waitMilliseconds(200);
+
+    colorWipe(&head_strip, normalColor, 1, true, COLOR_WIPE_TYPE::ALL);
+    colorWipe(&cockpit_strip, normalColor, 1, false, COLOR_WIPE_TYPE::ALL);
+
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  case 5:
+    // Screen Off
+    colorWipe(&head_strip, COLOR_BLACK, 1, true, COLOR_WIPE_TYPE::ALL);
+    colorWipe(&cockpit_strip, COLOR_BLACK, 1, false, COLOR_WIPE_TYPE::ALL);
+
+    waitMilliseconds(200);
+
+    colorWipe(&head_strip, normalColor, 1, true, COLOR_WIPE_TYPE::ALL);
+    colorWipe(&cockpit_strip, normalColor, 1, false, COLOR_WIPE_TYPE::ALL);
+    break;
+  case 6:
+    analogWrite(TOP_PIN, 0);
+
+    head_strip.setBrightness(0);
+    cockpit_strip.setBrightness(0);
+  case 7:
+    break;
+  default:
+    step = 0; // Skip Case #0
+    break;
+  }
 }
 
 /**
@@ -136,14 +262,10 @@ void normal_form()
 void phoenix_form()
 {
 #ifdef _DEBUG
-      Serial.println("Process - PHOENIX!");
+  Serial.println("Process - PHOENIX!");
 #endif
 
-#ifdef _DEBUG
-  dfmp3.setVolume(10);
-#else
-  dfmp3.setVolume(20);
-#endif
+  dfmp3.setVolume(VOLUME);
   delay(100);
 
   dfmp3.playFolderTrack16(2, 1);
@@ -167,25 +289,25 @@ void phoenix_form()
 class Mp3Notify
 {
 public:
-  static void PrintlnSourceAction(DfMp3_PlaySources source, const char* action)
+  static void PrintlnSourceAction(DfMp3_PlaySources source, const char *action)
   {
 #ifdef _DEBUG
-    if (source & DfMp3_PlaySources_Sd) 
+    if (source & DfMp3_PlaySources_Sd)
     {
-        Serial.print("SD Card, ");
+      Serial.print("SD Card, ");
     }
-    if (source & DfMp3_PlaySources_Usb) 
+    if (source & DfMp3_PlaySources_Usb)
     {
-        Serial.print("USB Disk, ");
+      Serial.print("USB Disk, ");
     }
-    if (source & DfMp3_PlaySources_Flash) 
+    if (source & DfMp3_PlaySources_Flash)
     {
-        Serial.print("Flash, ");
+      Serial.print("Flash, ");
     }
     Serial.println(action);
 #endif
   }
-  static void OnError(DfMp3& mp3, uint16_t errorCode)
+  static void OnError(DfMp3 &mp3, uint16_t errorCode)
   {
 #ifdef _DEBUG
     // see DfMp3_Error for code meaning
@@ -222,22 +344,22 @@ public:
     }
 #endif
   }
-  static void OnPlayFinished(DfMp3& mp3, DfMp3_PlaySources source, uint16_t track)
+  static void OnPlayFinished(DfMp3 &mp3, DfMp3_PlaySources source, uint16_t track)
   {
 #ifdef _DEBUG
     Serial.print("Play finished for #");
     Serial.println(track);
 #endif
   }
-  static void OnPlaySourceOnline(DfMp3& mp3, DfMp3_PlaySources source)
+  static void OnPlaySourceOnline(DfMp3 &mp3, DfMp3_PlaySources source)
   {
     PrintlnSourceAction(source, "online");
   }
-  static void OnPlaySourceInserted(DfMp3& mp3, DfMp3_PlaySources source)
+  static void OnPlaySourceInserted(DfMp3 &mp3, DfMp3_PlaySources source)
   {
     PrintlnSourceAction(source, "inserted");
   }
-  static void OnPlaySourceRemoved(DfMp3& mp3, DfMp3_PlaySources source)
+  static void OnPlaySourceRemoved(DfMp3 &mp3, DfMp3_PlaySources source)
   {
     PrintlnSourceAction(source, "removed");
   }
